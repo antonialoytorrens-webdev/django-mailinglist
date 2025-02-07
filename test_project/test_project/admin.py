1391319928ede10f7afe54b0329e63a5ec5dda50
Antoni Aloy Torrens <antoni_aloy@ikaue.com>
2025-02-07 12:11:28 +0100

    Remove AdHocUsers property

--- test_project/test_project/admin.py
+++ test_project/test_project/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-
-from test_project import models
-
-
-@admin.register(models.AdHocUserModel)
-class AdHocUserModelAdmin(admin.ModelAdmin):
-    model = models.AdHocUserModel
-    list_display = ("first_name", "last_name", "email")
